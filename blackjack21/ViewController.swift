315fea1c3984ba05ec4d9ee86cfd758afca330a0
Ivan Lai <229705jack2gmail.com>
2021-05-24 11:03:23 +0800

    Initial Commit

---
+++ blackjack21/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  blackjack21
+//
+//  Created by Lai Po Ying on 2021/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
